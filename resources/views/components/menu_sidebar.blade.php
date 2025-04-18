e72531cefecf753e4806d096e62766177f335abf
Fer <fernandosevilla028@gmail.com>
2025-04-18 21:06:23 +0200

    mejorado menú con logo tanto para modo claro como para el oscuro

--- resources/views/components/menu_sidebar.blade.php
+++ resources/views/components/menu_sidebar.blade.php
@@ -9,14 +9,28 @@
     <span class="sr-only">Abrir menú</span>
     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
         <path clip-rule="evenodd" fill-rule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"/>
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"/>
     </svg>
 </button>
 
 <aside id="separator-sidebar"
        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
        aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-[#262626]">
+    <nav class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-[#262626]">
+        <div class="flex justify-center mb-4">
+            {{-- Logo para modo claro --}}
+            <img src="{{ asset('img/logo_infogestion_modoClaro.webp') }}"
+                alt="Logo InfoGestión claro"
+                class="h-16 object-contain block dark:hidden" />
+
+            {{-- Logo para modo oscuro --}}
+            <img src="{{ asset('img/logo_infogestion_modoOscuro.webp') }}"
+                alt="Logo InfoGestión oscuro"
+                class="h-16 object-contain hidden dark:block" />
+        </div>
+
+        <hr class="border-t border-gray-300 dark:border-gray-600 mb-4">
+
         <ul class="space-y-2 font-medium">
             @foreach ($menu as $item)
                 @if (isset($item['type']) && $item['type'] === 'header')
@@ -45,5 +59,5 @@
                 @endif
             @endforeach
         </ul>
-    </div>
+    </nav>
 </aside>
