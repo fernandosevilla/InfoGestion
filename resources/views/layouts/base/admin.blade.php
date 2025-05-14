793a1bc63501d4748f75c95a8691f763a93fde78
Fer <fernandosevilla028@gmail.com>
2025-05-14 17:23:14 +0200

    Arreglado problema de como se veía en el móvil y añadida nueva sección para encabezados de las páginas

--- resources/views/layouts/base/admin.blade.php
+++ resources/views/layouts/base/admin.blade.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Panel de Administración')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicons/favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 
-<body class="min-h-screen bg-gray-100">
+<body class="min-h-screen bg-gray-100 dark:bg-[#303030]">
 
     {{-- Aquí colgamos el menú + drawer backdrop: --}}
     @include('components.menu_sidebar')
@@ -21,6 +22,12 @@
         {{-- contenido principal --}}
         <main class="flex-1 overflow-y-auto">
             <div class="container mx-auto px-6 py-6">
+                @hasSection('content_header')
+                    <div class="mb-6">
+                        @yield('content_header')
+                    </div>
+                @endif
+
                 @yield('content')
             </div>
         </main>
